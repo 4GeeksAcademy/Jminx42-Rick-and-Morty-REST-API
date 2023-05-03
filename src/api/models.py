c3734408d5cb3f3485420af991fea346686a37be
Jminx42 <105382626+Jminx42@users.noreply.github.com>
2023-05-03 19:41:48 +0000

    first commit, examples following with class

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    pets = db.relationship('Pet', backref ="user")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +16,20 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+
             # do not serialize the password, its a security breach
+        }
+    
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    age = db.Column(db.Integer, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "hello": "WOOOOF"
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Pet
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,73 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/pets', methods=['GET'])
+def get_all_pets():
+
+    pets = Pet.query.all()
+
+    pets_serialized = [pet.serialize() for pet in pets]
+    print("@@@@@@@@@@@@@@@@@@@")
+    print(pets)
+    print(pets_serialized)
+    print("@@@@@@@@@@@@@@@@@@@")
+
+    return jsonify({"pets": pets_serialized}), 200
+
+@api.route('/pets/<int:pet_id>', methods=['GET'])
+def get_pet_by_id(pet_id):
+
+    pet = Pet.query.get(pet_id)
+    if not pet:
+        return jsonify({"error": "No pet found with this id"}), 400
+    return jsonify({"pet": pet.serialize()}), 200
+    # if pet:
+    #     return jsonify({"pet": pet.serialize()}), 200
+    # else:
+    #     return jsonify({"error": "No pet found with this id"}), 400
+
+@api.route('/pet_by_name/<string:pet_name>', methods=['GET'])
+def get_pet_by_name(pet_name):
+
+    pet = Pet.query.filter_by(name = pet_name).first()
+
+    # pet = Pet.query.filter(Pet.name == pet_name).all()
+
+    if not pet:
+        return jsonify({"error": "No pet found with this name"}), 400
+    return jsonify({"pet": pet.serialize()}), 200
+    
+@api.route('/pet', methods=['POST'])
+def create_pet():
+
+    body = request.json
+    new_pet = Pet(name= body['name'], age= body["age"], user_id = body['user_id'])
+    db.session.add(new_pet)
+    db.session.commit()
+
+    return jsonify({"pet": "created"}), 200
+
+@api.route('/pet/<int:pet_id>', methods=['PUT'])
+def update_pet(pet_id):
+
+    body = request.json
+    pet = Pet.query.get(pet_id)
+    if not pet:
+        return jsonify({"error": "No pet found with this id"}), 400
+    pet.name = body['name']
+    pet.age = body['age']
+
+    db.session.commit()
+
+    return jsonify({"pet": "updated"}), 200
+
+@api.route('/pet/<int:pet_id>', methods=['DELETE'])
+def delete_pet(pet_id):
+    pet = Pet.query.get(pet_id)
+    if not pet:
+        return jsonify({"error": "No pet found with this id"}), 400
+    db.session.delete(pet)
+    db.session.commit()
+
+    return jsonify({"pet": pet.name + " was deleted"}), 200
