aa4baa1b90ee0e7b6b40266566bede9529fe9f41
Jminx42 <105382626+Jminx42@users.noreply.github.com>
2023-05-05 19:32:14 +0000

    finished models.py for the most part, still need to sort out endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,89 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # has_favorite = db.Column(db.Boolean, unique=False)
+    favorite = db.relationship('Favorite', backref ="user")
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.username}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username
             # do not serialize the password, its a security breach
         }
+    
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(120), nullable=False)
+    dimension = db.Column(db.String(120), nullable=False)
+    favorite = db.relationship('Favorite', backref ="location")
+
+    def __repr__(self):
+        return f'<Location {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "dimension": self.dimension
+        }
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    status = db.Column(db.String(120), nullable=False)
+    species = db.Column(db.String(120), nullable=False)
+    favorite = db.relationship('Favorite', backref ="character")
+
+    def __repr__(self):
+        return f'<Character {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "status": self.status,
+            "species": self.species,
+        }
+    
+class Episode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    status = db.Column(db.String(120), nullable=False)
+    favorite = db.relationship('Favorite', backref ="episode")
+
+    def __repr__(self):
+        return f'<Episode {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "status": self.status,
+        }
+
+class Category(enum.Enum):
+    character = "character"
+    episode = "episode"
+    location = "location"
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # category = db.Column(db.Enum(Category), server_default="character")
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episode.id'), nullable=True)
+    
